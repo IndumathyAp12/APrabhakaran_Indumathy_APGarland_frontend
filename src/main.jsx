2a09144736dc0aab86afceb95c183b02be47678e
Indu Prabha <apindhuece12@gmail.com>
2024-06-23 16:29:11 -0400

    Error boundary file is created

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Router>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
+
