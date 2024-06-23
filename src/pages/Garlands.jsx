1616be4c7543fbb2d1c198009cb2a544c5cf4231
Indu Prabha <apindhuece12@gmail.com>
2024-06-23 16:20:22 -0400

    made some changes

--- src/pages/Garlands.jsx
+++ src/pages/Garlands.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useGarlandContext } from '../contexts/GarlandContext';
 import GarlandList from '../components/GarlandList';
+import GarlandForm from '../components/GarlandForm';
 
 const Garlands = () => {
   const { state, fetchGarlands } = useGarlandContext();
@@ -12,11 +13,12 @@ const Garlands = () => {
   const { garlands, loading, error } = state;
 
   return (
-    <div>
+    <div className="container">
       <h1>Garlands</h1>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
       {!loading && !error && <GarlandList garlands={garlands} />}
+      <GarlandForm />
     </div>
   );
 };
